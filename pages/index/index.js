f4dc3735957686974c63a5cc6dd37a18b463b063
FrancoYang <franco2003@163.com>
2018-03-15 20:52:17 +0800

    updated codes

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,34 @@ Page({
   data: {
     movies:[],
     page:1,
-    size:6
+    size:6,
+    loading:true
   },
 
+  //生命周期函数--页面加载时执行
   onLoad(){
     this.loadMovies()
-  }
+  },
   
+  //加载电影数据
   loadMovies(){
-    const{size,page}=this.date
+    const{size,page}=this.data
+
+    this.setData({loading:true})
+
+    //发起网络请求
     wx.request({
-      url: 'https:db.miaov.com/doubanapi/movies/info?page=${page}&size=${size}',
+      url: 'https://db.miaov.com/doubanapi/movies/info?page=${page}&size=${size}',
+      success:(res)=>{
+        console.log(res)
+        const{data}=res.data //这里用到了ES6的解构赋值
+        const movies=this.data.movies
+
+        for(let i=0;i<data.length;i+=2){
+          movies.push([data[i],data[i+1]?data[i+1]:null])
+        }
+        this.setData({movies,loading:false})
+      }
     })
   }
 })
