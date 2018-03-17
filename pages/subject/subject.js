2dd2c8f9d4c07d7b421f0b6a1c343b56f6c2e5ea
FrancoYang <franco2003@163.com>
2018-03-17 22:58:09 +0800

    updated codes

--- pages/subject/subject.js
+++ pages/subject/subject.js
@@ -1,66 +1,42 @@
 // pages/subject/subject.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    name:"",
+    chineseScore:"",
+    mathScore:"",
+    avg:"",
+    flag:true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  input1(e){
+    var input=e.detail.value;
+    this.setData({
+      name:input
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  input2(e) {
+    var input = e.detail.value;
+    if(!isNaN(input)){
+      this.setData({
+        chineseScore:input
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  input3(e) {
+    var input = e.detail.value;
+    if (!isNaN(input)) {
+      this.setData({
+        mathScore: input
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  test(){
+    if(this.data.name==""||this.data.chineseScore==""||this.data.mathScore==""){
+      return;
+    }
+    var result=(this.data.chineseScore*1+this.data.mathScore*1)/2;
+    this.setData({
+      avg:result,
+      flag:false
+    });
   }
 })
